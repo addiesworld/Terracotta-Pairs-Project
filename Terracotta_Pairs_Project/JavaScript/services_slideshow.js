365f3a33ad4b248673644379a08a0d7bcab5db8e
Skye Breeze <skyexwarren@gmail.com>
2020-08-18 10:33:10 +0100

    Fixed Slideshow, Image Testing
    Fixed the slideshow slides at each media query.
    Added image placeholders for About Us and Services

--- Terracotta_Pairs_Project/JavaScript/services_slideshow.js
+++ Terracotta_Pairs_Project/JavaScript/services_slideshow.js
@@ -5,15 +5,25 @@
 
 const imagepathURL = [
     "../Images/Services_Images/Wedding_Food.jpg",
-    "..Images/Services_Images/Birthday_Food.jpg",
-    "../Images/Services_Images/Corporate_Food.jpg"
+    "../Images/Services_Images/Birthday_Food.jpg",
+    "../Images/Services_Images/Corporate_Food.jpeg"
 ]
 
 const slides = document.getElementsByClassName("image_slide")
-console.log(slides)
+// console.log(slides)
 document.addEventListener("DOMContentLoaded", function(){
     for (var i = 0; i< imagepathURL.length; i++) {
-        console.log(slides[i])
-        // slides[i].src = imagepathURL[i]
+        slides[i].src = imageValid(imagepathURL[i])
     }
 })
+
+let imageValid = function(imageURL) {
+    let image = new Image()
+    let urlImage = imageURL
+    image.src= urlImage
+    if (image.width == 0) 
+        return "../Images/Services_Images/Placeholder_Image.jpg"
+    else 
+        return imageURL
+}
+
--- Terracotta_Pairs_Project/JavaScript/team_members.js
+++ Terracotta_Pairs_Project/JavaScript/team_members.js
@@ -1,22 +1,23 @@
 
 
 class TeamMember {
-    constructor(stafffirstname, staffsurname, position, imageURL, description) {
+    constructor(stafffirstname, staffsurname, position, imageURL, description, sex) {
         this.stafffirstname = stafffirstname;
         this.staffsurname = staffsurname;
         this.position = position;
         this.imageURL = imageURL;
         this.description = description;
+        this.sex = sex;
     }
 }
 
 const Staff = [
-    Steve = new TeamMember("Steve", "Harrington", "Dingus", "../Images/About_Us_Images/Steve_Harrington.jpg", "Exercitation ea quis incididunt proident ipsum in. In deserunt veniam eu aliquip. Do deserunt nisi ullamco irure elit et voluptate nulla."),
-    Nancy = new TeamMember("Nancy", "Wheeler", "Badass", "../Images/About_Us_Images/Nancy_Wheeler.png", "Eiusmod ullamco aliqua ullamco consequat commodo non. Dolor ea Lorem nostrud eiusmod adipisicing adipisicing.  Eu aliquip laboris commodo dolore ut voluptate eiusmod aliqua quis eiusmod irure aute ullamco ut."),
-    Dustin = new TeamMember("Dustin", "Henderson", "Nerd", "../Images/About_Us_Images/Dustin.png", "Laborum dolor esse tempor Lorem nulla ad dolor.  Ex esse sint reprehenderit dolore fugiat enim eiusmod nisi commodo anim et excepteur fugiat."),
-    Eleven = new TeamMember("Eleven", "Hopper", "GateKeeper", "../Images/About_Us_Images/Eleven.jpg", "Consectetur ea reprehenderit quis duis magna sit cillum. Consectetur consequat nulla ad dolore pariatur esse id officia dolore elit nostrud minim est."),
-    Hopper = new TeamMember("Jim", "Hopper", "Security", "../Images/About_Us_Images/Hopper.jpeg", "Fugiat do id pariatur ut. Elit et sint cupidatat labore. Cupidatat adipisicing elit eu laborum magna velit tempor tempor cillum enim minim."),
-    Joyce = new TeamMember("Joyce", "Byers", "Legend", "../Images/About_Us_Images/Joyce.jpg", "Dolore aliquip ullamco nostrud ipsum est nostrud ad duis laboris esse magna. Amet cillum amet ex id voluptate reprehenderit in officia ex dolor amet occaecat.")
+    Steve = new TeamMember("Steve", "Harrington", "Dingus", "../Images/About_Us_Images/Steve_Harrington.jpg", "Exercitation ea quis incididunt proident ipsum in. In deserunt veniam eu aliquip. Do deserunt nisi ullamco irure elit et voluptate nulla.", "Male"),
+    Nancy = new TeamMember("Nancy", "Wheeler", "Badass", "../Images/About_Us_Images/Nancy_Wheeler.png", "Eiusmod ullamco aliqua ullamco consequat commodo non. Dolor ea Lorem nostrud eiusmod adipisicing adipisicing.  Eu aliquip laboris commodo dolore ut voluptate eiusmod aliqua quis eiusmod irure aute ullamco ut.", "Female"),
+    Dustin = new TeamMember("Dustin", "Henderson", "Nerd", "../Images/About_Us_Images/Dustin.png", "Laborum dolor esse tempor Lorem nulla ad dolor.  Ex esse sint reprehenderit dolore fugiat enim eiusmod nisi commodo anim et excepteur fugiat.", "Male"),
+    Eleven = new TeamMember("Eleven", "Hopper", "GateKeeper", "../Images/About_Us_Images/Eleven.jpg", "Consectetur ea reprehenderit quis duis magna sit cillum. Consectetur consequat nulla ad dolore pariatur esse id officia dolore elit nostrud minim est.", "Female"),
+    Hopper = new TeamMember("Jim", "Hopper", "Security", "../Images/About_Us_Images/Hopper.jpeg", "Fugiat do id pariatur ut. Elit et sint cupidatat labore. Cupidatat adipisicing elit eu laborum magna velit tempor tempor cillum enim minim.", "Male"),
+    Joyce = new TeamMember("Joyce", "Byers", "Legend", "../Images/About_Us_Images/Joyce.jpg", "Dolore aliquip ullamco nostrud ipsum est nostrud ad duis laboris esse magna. Amet cillum amet ex id voluptate reprehenderit in officia ex dolor amet occaecat.", "Female")
 ]
 
 const staffContainer = document.getElementById("staff_container");
@@ -31,7 +32,7 @@ function CreateStaffObject(staff) {
     card_container.classList.add('card')
     let imagemarkup = `        
     <div class="card_image">
-        <img src="${staff.imageURL}" alt="${staff.stafffirstname} ${staff.staffsurname}" id="${staff.stafffirstname}_${staff.staffsurname}">
+        <img src="${imageTest(staff.imageURL, staff.sex)}" alt="${staff.stafffirstname} ${staff.staffsurname}" id="${staff.stafffirstname}_${staff.staffsurname}">
     </div>`
     card_container.innerHTML += imagemarkup
 
@@ -62,6 +63,22 @@ function CreateStaffObject(staff) {
             image.style.marginLeft= -half+"px"
         }
     }
+}
+
+function imageTest(imageURL, sex) {
+    let image = new Image()
+    let urlImage = imageURL
+    // let urlImage = "sfdgfd"
+    image.src= urlImage
+    if (image.width == 0) {
+        if (sex == "Male") {
+            return "../Images/About_Us_Images/Male_Placeholder.jpg"
+        } else if (sex == "Female") {
+            return "../Images/About_Us_Images/Female_Placeholder.jpg"
+        }
+    }
+    else 
+        return imageURL
 }
 
 /* #region Card Template Layout
