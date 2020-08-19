30203f7f97a90b661e38866abd32e841608f7914
Skye Breeze <skyexwarren@gmail.com>
2020-08-19 01:25:02 +0100

    Removed Dead Code and Added typingSpeed variable

--- Terracotta_Pairs_Project/JavaScript/Testimonials/testimonials.js
+++ Terracotta_Pairs_Project/JavaScript/Testimonials/testimonials.js
@@ -5,20 +5,9 @@ $('.autoplay').slick({
     autoplaySpeed: 3000,
   });
 
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementsByClassName("testimonial_text").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
+const typingSpeed = 150;
 var txt = ['"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut lectus sem."',
-'"Affogato viral shabby chic, iPhone af four loko trust fund sunt next level scenester tousled austin. Fam godard ethical slow-carb vape."',
+'"Affogato viral shabby chic, ello governor! iPhone af four loko trust fund sunt next level scenester tousled austin. Fam godard ethical slow-carb vape."',
 '"Donec non felis feugiat felis gravida accumsan. Duis orci mauris, luctus sit amet lectus at, pellentesque aliquet tellus."'];
 
 let texts = document.getElementsByClassName("testimonial_text");
@@ -33,11 +22,11 @@ async function addCharx5(item, index) {
   for (var y = 0; y < txt[index].length; y++) {
     item.innerHTML += txt[index].charAt(y); 
     item.innerHTML += " |"
-    await sleep(50)
+    await sleep(typingSpeed)
     var stringTest = item.innerHTML.toString()
     var test = stringTest.substring(0, y+1)
     item.innerHTML = test
-    await sleep(25)
+    await sleep(typingSpeed)
   }
 }
 
