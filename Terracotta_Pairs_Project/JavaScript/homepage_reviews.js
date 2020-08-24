3fecf6850a002d6813d9715dfa0be11fb04b2325
Skye Breeze <skyexwarren@gmail.com>
2020-08-24 18:48:34 +0100

    Final Changes

---
+++ Terracotta_Pairs_Project/JavaScript/homepage_reviews.js
@@ -0,0 +1,10 @@
+
+$('.review_slick').slick({ 
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 3000,
+    prevArrow: false,
+    nextArrow: false,
+})
